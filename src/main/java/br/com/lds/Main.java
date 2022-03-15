8ae978e0eda26a846d05e81f55d3ee33f9d60bce
enthonyedu <enthonyedu@gmail.com>
2022-03-14 22:47:51 -0300

    feat: projeto finalizado

--- src/main/java/br/com/lds/Main.java
+++ src/main/java/br/com/lds/Main.java
@@ -1,5 +1,10 @@
 package br.com.lds;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -10,6 +15,30 @@ public class Main {
 	
 	private void start() {
 		
+		List<Integer> lista = new ArrayList<Integer>();
+		lista.add(10);
+		lista.add(30);
+		lista.add(40);
+		lista.add(60);
+		lista.add(70);
+		
+		for (int i = 0; i < lista.size(); i++) {
+			System.out.println("Item da lista: " + lista.get(i));
+			
+		}
+		
+		Map<Integer, String> mapa = new HashMap<Integer, String>();
+		mapa.put(1, "FAI");
+		mapa.put(2, "ETE");
+		mapa.put(3, "Inatel");
+		mapa.put(4, "Luis Pinto");
+		mapa.put(5, "Grupao");
+		mapa.put(6, "Sanico");
+		mapa.put(7, "Colegio");
+		
+		for (String valorDoMapa : mapa.values()) {
+			System.out.println("Valor do mapa: " + valorDoMapa);
+		}
 	}
 
 }
